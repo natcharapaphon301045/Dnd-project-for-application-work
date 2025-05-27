f496adaac63fc991733ecccba5a4974ba7fdc86d
natcharapaphon301045 <6431503019@lamduan.mfu.ac.th>
2025-05-28 02:41:05 +0700

    add vue 3 and vuetify

---
+++ Dnd project for application work/dnd-frontend/src/components/MainPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-container>
+        <h1>Welcome to DnD Project Main Page</h1>
+        <!-- ใส่ UI ของหน้าหลักที่นี่ -->
+    </v-container>
+</template>
+
+<script setup>
+</script>

fc43ffc9be65a5274247d47cab7504cfa72b182e
natcharapaphon301045 <6431503019@lamduan.mfu.ac.th>
2025-05-28 14:55:16 +0700

    New Front emd

--- Dnd project for application work/dnd-frontend/src/components/MainPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <v-container>
-        <h1>Welcome to DnD Project Main Page</h1>
-        <!-- ใส่ UI ของหน้าหลักที่นี่ -->
-    </v-container>
-</template>
-
-<script setup>
-</script>
